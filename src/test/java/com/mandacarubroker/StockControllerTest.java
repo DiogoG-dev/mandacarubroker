1be483fa75cab57b36c1c657849d2b73da4bbe4f
Diogo Gabriel <145854574+DiogoG-dev@users.noreply.github.com>
2024-02-18 10:22:28 -0300

    Update StockControllerTest.java
    Importando as bibliotecas e diretórios necessários para a criação dos testes.

--- src/test/java/com/mandacarubroker/StockControllerTest.java
+++ src/test/java/com/mandacarubroker/StockControllerTest.java
@@ -1,1 +1,14 @@
+import com.mandacarubroker.controller.StockController;
+import com.mandacarubroker.domain.stock.RequestStockDTO;
+import com.mandacarubroker.domain.stock.Stock;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
