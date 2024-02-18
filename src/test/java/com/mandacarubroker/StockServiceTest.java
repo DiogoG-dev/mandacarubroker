f1e8bd3b397562fc187eb3375953ed3907b34835
Diogo Gabriel <145854574+DiogoG-dev@users.noreply.github.com>
2024-02-18 10:58:27 -0300

    Update StockServiceTest.java
    Importando as bibliotecas e diretórios necessários para a criação dos testes.

--- src/test/java/com/mandacarubroker/StockServiceTest.java
+++ src/test/java/com/mandacarubroker/StockServiceTest.java
@@ -1,1 +1,9 @@
+import com.mandacarubroker.service.StockService;
+import com.mandacarubroker.domain.stock.RequestStockDTO;
+import com.mandacarubroker.domain.stock.Stock;
+import com.mandacarubroker.domain.stock.StockRepository;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.util.Optional;
